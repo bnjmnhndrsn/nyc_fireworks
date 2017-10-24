5a9de65b1f69f1c9d1620926c84fd30a60c2005e
Ben <benjamin.c.henderson@gmail.com>
2017-10-23 23:33:27 -0400

    get tweet command sort of working

--- fireworks/management/commands/tweet_updates.py
+++ fireworks/management/commands/tweet_updates.py
@@ -1,99 +1,83 @@
-import tweepy
+import datetime
+import time
+import pytz
 
 from django.core.management.base import BaseCommand, CommandError
 from django.utils import timezone
+from django.db.models import Q
 
 from fireworks.models import Firework
+from fireworks.twitter import tweet
 
 eastern = pytz.timezone('US/Eastern')
 
-TARGET_URL = 'http://www1.nyc.gov/nyc-resources/service/206/fireworks-displays'
-
-# breaking this out into a seperate function to make testing easier
-def get_request_text():
-    r = requests.get(TARGET_URL)
-    r.raise_for_status()
-    return r.text
-
 class Command(BaseCommand):
-    help = 'Closes the specified poll for voting'
-
-    def _log(self, text):
-        if self.options['verbosity'] > 1:
-            print text
-
-    def get_text(self):
-        text = get_request_text()
-        soup = BeautifulSoup(text, 'html.parser')
-        try:
-            richtext_el_text = soup.find(class_="richtext").get_text()
-        except AttributeError:
-            raise RuntimeError('Cannot find DOM node containing fireworks info')
-
-        return richtext_el_text
-
-    def parse_text(self, text):
-        rows = text.split('\n')[1:]
-        rows = [row.strip() for row in rows if row.strip()]
-        if len(rows) % 3 != 0:
-            raise RuntimeError('Incorrect number of lines')
-
-        result = []
-        for i in range(0, len(rows) - 2, 3):
-            result.append(self.parse_firework(*rows[i:i+3]))
-
-        return result
-
-    def parse_firework(self, date, location, sponsor):
-        try:
-            sponsor_name = re.search('Sponsor: (.+)', sponsor).group(1)
-        except AttributeError:
-            sponsor_name = ''
-
-        return {
-            'event_at': eastern.localize(parser.parse(date)),
-            'location': location,
-            'sponsor': sponsor_name
-        }
-
-    def create_or_update_fireworks(self, data):
-        now = timezone.now()
-        upcoming_fireworks = Firework.objects.filter(event_at__gte=now, cancelled=False)
-        found_firework_ids = []
-        for item in data:
-            if item['event_at'] < now:
-                continue
-
-            try:
-                found = upcoming_fireworks.get(**item)
-                found_firework_ids.append(found.pk)
-                self._log('found existing firework %s' % found)
-            except Firework.DoesNotExist:
-                firework = Firework(**item)
-                self._log('creating new firework %s' % firework)
-                if not self.options['dry_run']:
-                    firework.save()
-                    found_firework_ids.append(firework.pk)
-
-        not_found_fireworks = upcoming_fireworks.exclude(id__in=found_firework_ids)
-        if len(not_found_fireworks):
-            not_found_string = '\n'.join([str(firework) for firework in not_found_fireworks])
-            self._log('could not find fireworks:\n%s' % not_found_string)
-            if not self.options['dry_run']:
-                not_found_fireworks.update(cancelled=True)
-
-
+    help = 'Tweet out upcoming fireworks'
+    
+    REMINDER_INTERVALS = (14, 7, 3, 1, 0)
+    
+    def tweet(self, message):
+        if self.options['dry_run']:
+            print message
+        else:
+            tweet(message)
+            time.sleep(60)
+    
     def add_arguments(self, parser):
         parser.add_argument(
             '--dry-run',
             action='store_true',
             dest='dry_run',
             default=False,
-            help='Print scraped data instead of saving',
+            help='Print out tweets instead of actually tweeting',
         )
+    
+    def get_reminder_fireworks(self):
+        today = timezone.now().date()
+        time_query = Q()
+        for interval in self.REMINDER_INTERVALS:
+            time_query = time_query | Q(event_at__date=today + datetime.timedelta(days=interval))
+        
+        return Firework.objects.filter(cancelled=False).filter(time_query)            
 
+    def get_new_fireworks(self):
+        today = timezone.now().date()
+        return Firework.objects.filter(cancelled=False, created_at__date=today)
+    
+    def get_cancelled_fireworks(self):
+        today = timezone.now().date()
+        return Firework.objects.filter(cancelled=True, updated_at__date=today)
+                
+    def get_message(self, prefix, firework):
+        return '%s: %s on %s. Sponsored by %s' % (
+            prefix, firework.location, firework.event_at.strftime('%b %d at %I:%M %p'), firework.sponsor
+        )
+    
     def handle(self, *args, **options):
         self.options = options
-        text = self.get_text()
-        parsed = self.parse_text(text)
-        self.create_or_update_fireworks(parsed)
+        
+        timezone.activate(eastern)
+        
+        new_fireworks = self.get_new_fireworks()
+        cancelled_fireworks = self.get_cancelled_fireworks()
+        reminder_fireworks = self.get_reminder_fireworks()
+        
+        if self.options['verbosity'] > 1:
+            print 'New Fireworks: %s' % len(new_fireworks)
+            print 'Cancelled Fireworks: %s' % len(cancelled_fireworks)
+            print 'Reminder Fireworks: %s' % len(reminder_fireworks)   
+            
+        for firework in new_fireworks:
+            message = self.get_message('NEW', firework)
+            self.tweet(message)
+        
+        for firework in cancelled_fireworks:
+            message = self.get_message('CANCELLED', firework)
+            self.tweet(message)
+        
+        for firework in reminder_fireworks:
+            message = self.get_message('REMINDER', firework)
+            self.tweet(message)
+
+        
+        
